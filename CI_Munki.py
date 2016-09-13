b12393cf0c8118684278b89f6c7ae661fdafe34f
DDeRusha <dderusha@gmail.com>
2016-09-12 19:38:49 -0500

    Removed testing lines
    if munkiserver is str :
        print str(munkiserver)

--- CI_Munki.py
+++ CI_Munki.py
@@ -9,8 +9,6 @@ import sys
 LOGGED_IN_USER = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; LOGGED_IN_USER = [LOGGED_IN_USER, ""][LOGGED_IN_USER in [u"loginwindow", None, u""]]; sys.stdout.write(LOGGED_IN_USER + "\n")
 munkiserver = raw_input("Enter munki server http://10.0.6.127/munki_repo: ")
 manifest = raw_input("Enter manifest: ")
-if munkiserver is str :
-    print str(munkiserver)
 
 package = '/Users/' + LOGGED_IN_USER + '/munkitools-2.8.0.2810.pkg'
 
